7d3b83f56fc21a663a6a77cf0ca0402901e58f88
Đức Trịnh <ductrinh@192.168.40.104>
2023-12-13 03:28:18 +0700

    put random to quote

--- app/tim-tu/placeholder.tsx
+++ app/tim-tu/placeholder.tsx
@@ -5,10 +5,10 @@ export default async function Placeholder() {
   const randomSentence = await getRandomSentence();
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-2 italic">
+    <div className="flex h-full w-full flex-col items-center justify-center gap-2 px-6 italic">
       <LightBulbIcon className="h-10 w-10" />
       {randomSentence ? (
-        <span>{randomSentence}</span>
+        <span>{`"${randomSentence}"`}</span>
       ) : (
         <>
           <span>{'"Làm thơ thì phải có vần'}</span>
