d9b6f5847ecbdbdf93027062c300d8f1927cc79c
tdgiangjvl <giangtran@DESKTOP-6K7SDTT.localdomain>
2024-01-08 02:28:37 +0700

    adding tooltips

--- app/ui/rhyme/type-tab-menu.tsx
+++ app/ui/rhyme/type-tab-menu.tsx
@@ -29,42 +29,54 @@ export default function TypeTabMenu({
 
   return (
     <div className="hidden w-[23rem] justify-between md:flex">
-      <button
-        className={clsx(
-          "flex rounded-lg px-[1.25rem] py-[0.75rem] font-semibold transition-all duration-300",
-          {
-            "bg-blue-100 text-blue-500": currentType == SearchType.VanXuoi,
-            "text-gray-400 hover:text-gray-800":
-              currentType != SearchType.VanXuoi,
-          },
-        )}
-        onClick={handleChangeType(SearchType.VanXuoi)}>
-        Vần Xuôi
-      </button>
-      <button
-        className={clsx(
-          "flex rounded-lg px-[1.25rem] py-[0.75rem] font-semibold transition-all duration-300",
-          {
-            "bg-blue-100 text-blue-500": currentType == SearchType.VanDao,
-            "text-gray-400 hover:text-gray-800":
-              currentType != SearchType.VanDao,
-          },
-        )}
-        onClick={handleChangeType(SearchType.VanDao)}>
-        Vần Đảo
-      </button>
-      <button
-        className={clsx(
-          "flex rounded-lg px-[1.25rem] py-[0.75rem] font-semibold transition-all duration-300",
-          {
-            "bg-blue-100 text-blue-500": currentType == SearchType.NoiLai,
-            "text-gray-400 hover:text-gray-800":
-              currentType != SearchType.NoiLai,
-          },
-        )}
-        onClick={handleChangeType(SearchType.NoiLai)}>
-        Nói Lái
-      </button>
+      <div className="con-tooltip">
+        <button
+          className={clsx(
+            "flex rounded-lg px-[1.25rem] py-[0.75rem] font-semibold transition-all duration-300",
+            {
+              "bg-blue-100 text-blue-500": currentType == SearchType.VanXuoi,
+              "text-gray-400 hover:text-gray-800":
+                currentType != SearchType.VanXuoi,
+            },
+          )}
+          onClick={handleChangeType(SearchType.VanXuoi)}
+        >
+          Vần Xuôi
+        </button>
+        <span className="tooltip">Tìm vần đôi, vần ba, tư... VD: báo đời 	&#8594; bao lời </span>
+      </div>
+      <div className="con-tooltip">
+        <button
+          className={clsx(
+            "flex rounded-lg px-[1.25rem] py-[0.75rem] font-semibold transition-all duration-300",
+            {
+              "bg-blue-100 text-blue-500": currentType == SearchType.VanDao,
+              "text-gray-400 hover:text-gray-800":
+                currentType != SearchType.VanDao,
+            },
+          )}
+          onClick={handleChangeType(SearchType.VanDao)}
+        >
+          Vần Đảo
+        </button>
+        <span className="tooltip">Tìm vần đảo đôi, ba, tư... VD: báo đời 	&#8594; trời cao </span>
+      </div>
+      <div className="con-tooltip">
+        <button
+          className={clsx(
+            "flex rounded-lg px-[1.25rem] py-[0.75rem] font-semibold transition-all duration-300",
+            {
+              "bg-blue-100 text-blue-500": currentType == SearchType.NoiLai,
+              "text-gray-400 hover:text-gray-800":
+                currentType != SearchType.NoiLai,
+            },
+          )}
+          onClick={handleChangeType(SearchType.NoiLai)}
+        >
+          Nói Lái
+        </button>
+        <span className="tooltip">Tìm cách nói lái cho từ. VD : hai em cún &#8594; hun em cái</span>
+      </div>
     </div>
   );
 }
