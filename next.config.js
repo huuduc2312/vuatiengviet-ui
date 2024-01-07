314f7bb52bb3c3486c4bed97e2b548dc4d04a157
tdgiangjvl <giangtran@DESKTOP-6K7SDTT.localdomain>
2024-01-08 00:07:42 +0700

    redirect tim-tu

--- next.config.js
+++ next.config.js
@@ -16,6 +16,16 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/tim-tu',
+        destination: '/tim-van',
+        permanent: true, // Set to true for 301 redirect, or false for 302 redirect
+      },
+      // You can add more redirect rules here if needed
+    ];
+  },
 };
 
 module.exports = nextConfig;
