cc7632dbf0b56d910bffb5a4ee7b3cd6f61ff1e2
tdgiangjvl <giangtran@DESKTOP-6K7SDTT.localdomain>
2024-01-24 01:33:00 +0700

    adjust title and description

--- app/tim-van/page.tsx
+++ app/tim-van/page.tsx
@@ -13,17 +13,17 @@ const metadataMap: { [key in SearchType]: Metadata } = {
   "van-xuoi": {
     title: "Tìm Vần & Vần Đảo | Vần Ba, Vần Đôi, Vần Tư | Vựa Tiếng Việt",
     description:
-      "Công cụ Tìm Vần & Vần Đảo. Tìm Vần viết rap, làm thơ, viết nhạc, sáng tác một cách dễ dàng. Hỗ trợ tìm vần online, dành cho rapper, nhạc sĩ, nhà thơ và người yêu chữ.",
+      "Web Tìm Vần Đảo. Từ điển vần rap. làm thơ, viết nhạc, sáng tác một cách dễ dàng. Phần mềm tìm vần online, dành cho rapper, nhạc sĩ, nhà thơ và người yêu chữ.",
   },
   "van-dao": {
     title: "Tìm Vần & Vần Đảo | Tìm Vần Đảo Như Rang Lạc | Vựa Tiếng Việt",
     description:
-      "Công cụ Tìm Vần & Vần Đảo đột phá - lựa chọn hàng đầu cho người yêu thơ, rap và nhạc. Tại Vựa Tiếng Việt, bạn có thể tìm kiếm vần ba, vần đôi, vần đảo một cách nhanh chóng. Tận hưởng trải nghiệm tìm vần nhờ AI.",
+      "Web Tìm Vần Đảo đột phá. Từ điển vần đảo. Từ điển vần rap. Lựa chọn hàng đầu cho rapper và nhạc six. Tại Vựa Tiếng Việt, bạn có thể tận hưởng trải nghiệm tìm vần nhờ AI.",
   },
   "noi-lai": {
     title: "Công Cụ Tìm Vần & Nói Lái | Vựa Tiếng Việt",
     description:
-      "Phát huy vẻ đẹp của từ ngữ với Công Cụ Tìm Vần & Vần Đảo tại Vựa Tiếng Việt. Tìm cách nói lái. Tìm Vần đôi, vần ba, vần đảo, nói lái, nói trại một cách chính xác và sáng tạo.",
+      "Phát huy vẻ đẹp của từ ngữ với Web Tìm Vần & Vần Đảo tại Vựa Tiếng Việt. Tìm cách nói lái. Tìm Vần đôi, vần ba, vần đảo, nói lái, nói trại một cách chính xác và sáng tạo.",
   },
 };
 
